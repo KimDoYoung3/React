2421a426b730d60cbbf9e0877e09249da0b54b22
Doyoung <stdodentysoung@gmail.com>
2022-10-22 14:20:59 +0900

    2022.10.22
    TodoTemplate.jsx 생성

---
+++ src/components/TodoTemplate.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const TodoTemplateBlock = styled.div`
+`;
+
+function TodoTemplate() {
+    return (
+        <div>TodoTemplate</div>
+    )
+}
+
+export default TodoTemplate;
